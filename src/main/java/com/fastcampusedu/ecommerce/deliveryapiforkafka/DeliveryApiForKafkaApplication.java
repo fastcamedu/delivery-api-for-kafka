366c64e39533b7a43b1486bc11797c621f1542db
fastcamedu <fastcamedu@gmail.com>
2023-07-26 17:55:45 +0900

    Init Delivery Project for testing Kafka Pub-Sub

---
+++ src/main/java/com/fastcampusedu/ecommerce/deliveryapiforkafka/DeliveryApiForKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampusedu.ecommerce.deliveryapiforkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApiForKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryApiForKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampusedu/ecommerce/deliveryapiforkafka/DeliveryApiForKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampusedu.ecommerce.deliveryapiforkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiForKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
